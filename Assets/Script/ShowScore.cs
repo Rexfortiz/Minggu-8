7ff3a728086bf5226cc96c1516e935c72c167ada
Rexfortiz <daydreamer9422@gmail.com>
2020-11-17 16:50:11 +0700

    Minggu 8
    Thomas Farelino A R
    01918147226
    pge

---
+++ Assets/Script/ShowScore.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowScore : MonoBehaviour
+{
+    void FixedUpdate()
+    {
+        GetComponent<Text>().text = Data.score.ToString("000");
+
+    }
+}
